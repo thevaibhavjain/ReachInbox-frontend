931ba8ecd62270188f1465ac523b4fac8e0d868e
VAIBHAV <vaibhavjainadmirable@gmail.com>
2024-08-26 03:31:28 +0530

    AppBar pAGe

---
+++ src/components/AppBar.tsx
@@ -0,0 +1,17 @@
+import namelogo from "../assets/logo_name.svg";
+
+function AppBar() {
+  return (
+    <div className="border-[#25262B] bg-black border-b-2 fixed text-white h-16 w-screen flex items-center justify-center">
+      <div className="">
+        <img
+          src={namelogo}
+          alt="logo"
+          className="h-6"
+        ></img>
+      </div>
+    </div>
+  );
+}
+
+export default AppBar;
